906ed5eadc00bb0486cbdf6fceb56a1ec61accf7
ysh2987 <ysh2987@naver.com>
2021-08-19 23:47:32 +0900

    JavaScript Study

---
+++ project/vanilla js slider/main.js
@@ -0,0 +1,65 @@
+const slider_List = document.querySelector('.slider_List')
+const items = document.querySelectorAll('.item')
+const item_len = items.length; 
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const item_Width = 400;
+const slide_speed = 300;
+
+const itemfirst = document.querySelector('.item:first-child')
+const itemlast = document.querySelector('.item:last-child')
+const firstNode = itemfirst.cloneNode(true);
+const lastNode = itemlast.cloneNode(true);
+const active = "slide_active"
+let test = 0;
+let carindex
+carindex = test
+// carSlide.classList.add(active);
+
+firstNode.style.width = `${item_Width}px`
+lastNode.style.width = `${item_Width}px`
+slider_List.appendChild(firstNode);
+slider_List.insertBefore(lastNode, slider_List.firstElementChild);
+
+slider_List.style.width = `${item_Width*(item_len+2)}px`
+
+slider_List.style.transform = `translate(-${item_Width}px)`
+
+
+next.addEventListener('click', function(){
+    if(carindex  <= item_len -1 ){
+            slider_List.style.transition = slide_speed + "ms";
+            slider_List.style.transform = `translate(-${item_Width*(carindex +2)}px)`
+    }
+    if (carindex  === item_len-1){
+        setTimeout(function(){
+        slider_List.style.transition = '0s' 
+        slider_List.style.transform = `translate(-${item_Width}px,0)`
+    }, 300)
+        carindex  = -1
+        
+        
+    }
+    // carSlide.classList.remove(active);
+    ++carindex
+    // carSlide.classList.add(active);
+
+})
+
+
+prev.addEventListener('click', function(){
+    if(carindex >= 0){
+        slider_List.style.transition = slide_speed + "ms";
+        slider_List.style.transform = `translate(-${item_Width*(carindex)}px)`
+    }
+
+    if(carindex === 0){    
+        setTimeout(function(){
+        slider_List.style.transition = '0s' 
+        slider_List.style.transform = `translate(-${item_Width * item_len}px,0)`
+    }, 300)
+    carindex = item_len
+}
+--carindex
+})
+
