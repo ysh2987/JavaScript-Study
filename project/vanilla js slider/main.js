73a6bc90e7276794ebd5dae8424e241daffcee35
Ssong <81687138+ysh2987@users.noreply.github.com>
2021-08-22 18:36:56 +0900

    js-slider

--- project/vanilla js slider/main.js
+++ project/vanilla js slider/main.js
@@ -1,28 +1,21 @@
 const slider_List = document.querySelector('.slider_List')
 const items = document.querySelectorAll('.item')
-const item_len = items.length; 
 const next = document.querySelector('.next')
 const prev = document.querySelector('.prev')
 const item_Width = 400;
 const slide_speed = 300;
-
+const item_len = items.length; 
 const itemfirst = document.querySelector('.item:first-child')
 const itemlast = document.querySelector('.item:last-child')
 const firstNode = itemfirst.cloneNode(true);
 const lastNode = itemlast.cloneNode(true);
-const active = "slide_active"
-let test = 0;
-let carindex
-carindex = test
-// carSlide.classList.add(active);
 
-firstNode.style.width = `${item_Width}px`
-lastNode.style.width = `${item_Width}px`
+let carindex = 0;
+
+
 slider_List.appendChild(firstNode);
-slider_List.insertBefore(lastNode, slider_List.firstElementChild);
-
+slider_List.insertBefore(lastNode, slider_List.firstElementChild); // last Node를 첫번째 자식요소 
 slider_List.style.width = `${item_Width*(item_len+2)}px`
-
 slider_List.style.transform = `translate(-${item_Width}px)`
 
 
@@ -34,16 +27,11 @@ next.addEventListener('click', function(){
     if (carindex  === item_len-1){
         setTimeout(function(){
         slider_List.style.transition = '0s' 
-        slider_List.style.transform = `translate(-${item_Width}px,0)`
+        slider_List.style.transform = `translate(-${item_Width}px)`
     }, 300)
         carindex  = -1
-        
-        
     }
-    // carSlide.classList.remove(active);
-    ++carindex
-    // carSlide.classList.add(active);
-
+    carindex++
 })
 
 
@@ -52,14 +40,12 @@ prev.addEventListener('click', function(){
         slider_List.style.transition = slide_speed + "ms";
         slider_List.style.transform = `translate(-${item_Width*(carindex)}px)`
     }
-
     if(carindex === 0){    
         setTimeout(function(){
         slider_List.style.transition = '0s' 
-        slider_List.style.transform = `translate(-${item_Width * item_len}px,0)`
+        slider_List.style.transform = `translate(-${item_Width * item_len}px)`
     }, 300)
     carindex = item_len
 }
 --carindex
 })
-
